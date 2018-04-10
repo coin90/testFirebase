7cacb2e19550d62dac5dff7c477681caff51f23c
jccoin <jccoin@bravent.net>
2018-04-10 18:36:04 +0200

    Creación proyecto

---
+++ firebase/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace firebase
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
