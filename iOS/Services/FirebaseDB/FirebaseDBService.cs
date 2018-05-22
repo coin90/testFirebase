5a9c9ded9de86dd5bb34d42f3c6608db2be1e766
Javier Fernando Huamanchumo Arauco <javier.huamanchumo@gmail.com>
2018-05-22 17:48:02 +0200

    NSNull controlado

--- iOS/Services/FirebaseDB/FirebaseDBService.cs
+++ iOS/Services/FirebaseDB/FirebaseDBService.cs
@@ -17,57 +17,64 @@ using firebasesample.Models;
 [assembly: Dependency(typeof(FirebaseDBService))]
 namespace firebasesample.iOS.Services.FirebaseDB
 {
-    public class FirebaseDBService : IFirebaseDBService
-    {
-        public static String KEY_MESSAGE = "items";
+	public class FirebaseDBService : IFirebaseDBService
+	{
+		public static String KEY_MESSAGE = "items";
 
-        
-        private FirebaseAuthService authService = new FirebaseAuthService();
-        DatabaseReference databaseReference;
-        
-        public void Connect(){
-            databaseReference = Database.DefaultInstance.GetRootReference();
-        }
-        public void GetMessage(){
-          var userId = authService.GetUserId();
 
-           
+		private FirebaseAuthService authService = new FirebaseAuthService();
+		DatabaseReference databaseReference;
 
-            var messages = databaseReference.GetChild("items").GetChild(userId);
+		public void Connect()
+		{
+			databaseReference = Database.DefaultInstance.GetRootReference();
+		}
+		public void GetMessage()
+		{
+			var userId = authService.GetUserId();
 
-            nuint handleReference2 = messages.ObserveEvent(DataEventType.Value, (snapshot) => {
-                //var folderData = snapshot.GetValue<NSDictionary>();
-                // Do magic with the folder data
-                NSDictionary folderData = snapshot.GetValue<NSDictionary>();
-                var keys = folderData.Keys;
-                ObservableCollection<Homework> list = new ObservableCollection<Homework>();
-                foreach (var item in folderData)
-                {
-                    list.Add(new Homework
+
+
+			var messages = databaseReference.GetChild("items").GetChild(userId);
+
+			nuint handleReference2 = messages.ObserveEvent(DataEventType.Value, (snapshot) =>
+			{
+				//var folderData = snapshot.GetValue<NSDictionary>();
+				// Do magic with the folder data
+				NSObject value = snapshot.GetValue();
+				if(value is NSDictionary folderData)
+				{
+					var keys = folderData.Keys;
+                    ObservableCollection<Homework> list = new ObservableCollection<Homework>();
+                    foreach (var item in folderData)
                     {
-                        Key = item.Key.ToString(),
-                        HomeWork = item.Value.ToString()
-                    });
-                }
-                MessagingCenter.Send(FirebaseDBService.KEY_MESSAGE, FirebaseDBService.KEY_MESSAGE, list);
+                        list.Add(new Homework
+                        {
+                            Key = item.Key.ToString(),
+                            HomeWork = item.Value.ToString()
+                        });
+                    }
+                    MessagingCenter.Send(FirebaseDBService.KEY_MESSAGE, FirebaseDBService.KEY_MESSAGE, list);
+                }			           
+			});
+		}
+		public void SetMessage(String message)
+		{
+			var userId = authService.GetUserId();
+			var messages = databaseReference.GetChild("items").GetChild(userId).Reference;
+			var key = messages.GetChildByAutoId().Key;
+			messages.GetChild(key).SetValue((NSString)message);
+		}
+		public String GetMessageKey()
+		{
+			return KEY_MESSAGE;
+		}
 
-            });
-        }
-        public void SetMessage(String message){
-            var userId = authService.GetUserId();
-            var messages = databaseReference.GetChild("items").GetChild(userId).Reference;
-            var key = messages.GetChildByAutoId().Key;
-            messages.GetChild(key).SetValue((NSString)message);
-        }
-        public String GetMessageKey(){
-            return KEY_MESSAGE;
-        }
-
-        public void DeleteItem(string key)
-        {
-            var userId = authService.GetUserId();
-            var messages = databaseReference.GetChild("items").GetChild(userId).Reference;
-            messages.GetChild(key).RemoveValue();
-        }
-    }
+		public void DeleteItem(string key)
+		{
+			var userId = authService.GetUserId();
+			var messages = databaseReference.GetChild("items").GetChild(userId).Reference;
+			messages.GetChild(key).RemoveValue();
+		}
+	}
 }
